ebac8f8ce9b5747bf7b375a8b487e303e9b85f65
José E. Lozano <lozanoje@gmail.com>
2025-06-29 17:45:14 +0200

    Updates to localization
    Thanks to zjordi

--- fvtt-module-deltagreen-es.js
+++ fvtt-module-deltagreen-es.js
@@ -1,5 +1,5 @@
 Hooks.once('init', () => {
-  document.getElementById("logo").src = "/modules/fvtt-module-deltagreen-es/images/fvtt-deltagreen-es.webp";
+  // document.getElementById("logo").src = "/modules/fvtt-module-deltagreen-es/images/fvtt-deltagreen-es.webp";
 });
 
 
